8b2f9c8f82b2d39476c64596c97aeba5fc8fa974
Rahul <rahul@proinfocus.com>
2018-06-24 13:59:17 +0530

    Add project files.

---
+++ DataManager.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataManager.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
